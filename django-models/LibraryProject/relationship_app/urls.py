2593097db8328025020e08de4cebd8f77b0c9bac
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-21 07:35:34 -0400

    update the routes and add some functions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth import views as auth_views
+from .views import list_books, LibraryDetailView, RegisterView
+from .views import register_view
+
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+  path('books/', list_books, name='list_books'),
+  path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+     path('register/', register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,13 +3,32 @@ from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
+# relationship_app/views.py
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Function to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Adjust to your homepage name
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 
 # Class-based view to show details for a specific library
@@ -18,3 +37,9 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+
+
+class RegisterView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')  # redirect to login after registration
