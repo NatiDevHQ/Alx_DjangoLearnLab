ef367c0ee06b971f737bbe02b8f42580f92be2c3
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-27 07:22:24 -0400

    added  advanced_features_and_security folder

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,39 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register,
+    admin_view,
+    librarian_view,
+    member_view,
+    add_book,
+    edit_book,
+    delete_book
+)
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Registration route
+    path('register/', register, name='register'),
+
+    # Login/logout routes
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+
+    # Role-based views
+     path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
+      
+    path('books/add_book/', add_book, name='add_book'),
+    path('books/<int:book_id>/edit_book/', edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete_book/', delete_book, name='delete_book'),
+]
+
+
+
+
+
