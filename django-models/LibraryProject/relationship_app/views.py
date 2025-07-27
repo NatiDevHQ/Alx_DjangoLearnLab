b91ebacb8f75a23b9fe6cb3cfeb24e1cb4d58aa6
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-27 06:36:26 -0400

    debug

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,9 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test, login_required
 from .models import Library, Book, UserProfile
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render
+
 
 # Function to list all books
 def list_books(request):
@@ -52,7 +55,6 @@ class RegisterView(CreateView):
   # redirect to login after registration
 
 
-
 def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
@@ -62,14 +64,17 @@ def is_librarian(user):
 def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
+@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
+@login_required
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
