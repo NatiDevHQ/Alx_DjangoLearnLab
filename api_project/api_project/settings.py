0848889ccd7bfec8068c7e044b0c0fa16afdf22f
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-03 16:32:07 -0400

    token auth

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -28,6 +28,15 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ]
+}
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+     'rest_framework',
+    'rest_framework.authtoken',
     'api'
     
 ]
