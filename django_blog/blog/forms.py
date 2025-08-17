9bd7fc2059c0a0acfd589e90bb6dfe870e3a8cd4
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-17 16:44:30 -0400

    correct the form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,62 +1,76 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
-from .models import Post
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import Comment
-
-from django import forms
-from .models import Comment
-from django import forms
+from taggit.forms import TagWidget  # Import TagWidget from taggit
 from .models import Post, Comment, Tag
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(required=False, help_text="Comma-separated tags")
-
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-
-    def save(self, commit=True, *args, **kwargs):
-        post = super().save(commit=False)
-        if commit:
-            post.save()
-        # Handle tags
-        tags_str = self.cleaned_data['tags']
-        if tags_str:
-            tag_names = [t.strip() for t in tags_str.split(',')]
-            post.tags.clear()
-            for name in tag_names:
-                tag, created = Tag.objects.get_or_create(name=name)
-                post.tags.add(tag)
-        return post
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter post title'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5,
+                'placeholder': 'Write your post content here...'
+            }),
+            'tags': TagWidget(attrs={
+                'class': 'form-control',
+                'placeholder': 'Add comma-separated tags',
+                'data-role': 'tagsinput'  # Optional for better tag input UI
+            })
+        }
+        help_texts = {
+            'tags': 'Comma-separated list of tags'
+        }
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'rows':3, 'placeholder':'Write a comment...'}), label='')
-
     class Meta:
         model = Comment
         fields = ['content']
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
-
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Write your comment here...'
+            })
+        }
+        labels = {
+            'content': ''
+        }
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'autocomplete': 'email'
+        })
+    )
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
-
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'autocomplete': 'username'
+            }),
+        }
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("first_name", "last_name", "email")
-
-
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'autocomplete': 'email'
+            }),
+        }
